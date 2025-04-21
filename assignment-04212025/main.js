72cdd50f65b8ee67ce3bd5f33446f6252869f27d
Sunil Park <sunilpark1129@gmail.com>
2025-04-21 19:50:09 -0400

    first commit

---
+++ assignment-04212025/main.js
@@ -0,0 +1,106 @@
+// Sunil Park
+
+// implement my own version of these array instance methods: map, filter, reduce, includes, find, slice
+
+// test case
+const arr = [1, 2, 3, 4, 5];
+
+/*==================================================================*/
+
+// map
+
+arr.__proto__.myMap = function (cb) {
+  const r = [];
+  for (let i = 0; i < this.length; i++) {
+    r.push(cb(this[i], i, this));
+  }
+  return r;
+};
+const p1 = arr.myMap((x) => x + 2);
+console.log(p1); // [3, 4, 5, 6, 7]
+
+/*==================================================================*/
+
+/*==================================================================*/
+
+// filter
+
+arr.__proto__.myFilter = function (cb) {
+  const r = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i], i, this)) {
+      r.push(this[i]);
+    }
+  }
+  return r;
+};
+const p2 = arr.myFilter((x) => x < 3);
+console.log(p2); // [1, 2]
+
+/*==================================================================*/
+
+/*==================================================================*/
+
+// reduce
+
+arr.__proto__.myReduce = function (cb, initialValue) {
+  const temp = [...arr];
+  // if parameter has second argument
+  if (initialValue) temp.unshift(initialValue);
+  // r => first item
+  let r = temp[0];
+
+  for (let i = 1; i < temp.length; i++) {
+    r = cb(r, this[i]);
+  }
+  return r;
+};
+const p3 = arr.myReduce((a, b) => (a > b ? a : b));
+console.log(p3); // 5
+
+/*==================================================================*/
+
+/*==================================================================*/
+
+// includes
+
+arr.__proto__.myInclude = function (x) {
+  for (let i = 0; i < this.length; i++) {
+    if (x === this[i]) return true;
+  }
+  return false;
+};
+const p4 = arr.myInclude(3);
+console.log(p4); // true
+
+/*==================================================================*/
+
+/*==================================================================*/
+
+// find
+
+arr.__proto__.myFind = function (cb) {
+  for (let i = 0; i < this.length; i++) {
+    if (cb(this[i])) return this[i];
+  }
+};
+const p5 = arr.myFind((x) => x > 3);
+console.log(p5); // 4
+
+/*==================================================================*/
+
+/*==================================================================*/
+
+// slice
+
+arr.__proto__.mySlice = function (a, b) {
+  let r = [];
+  for (let i = a; i < Math.min(b, this.length); i++) {
+    r.push(this[i]);
+  }
+  return r;
+};
+const p6 = arr.mySlice(2, 7);
+console.log(p6); // [3, 4, 5];
+
+/*==================================================================*/
